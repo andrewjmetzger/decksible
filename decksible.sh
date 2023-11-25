a3972fffd52d1671fe1cf4e2b8094bba304fc9f3
Andrew Metzger <andrewjmetzger@users.noreply.github.com>
2023-11-25 11:59:13 -0500

    Create main playbook.

--- decksible.sh
+++ decksible.sh
@@ -48,10 +48,7 @@ cd "${decksible_path}" || exit
 
 ansible-galaxy install -r ${decksible_path}/requirements.yml
 
-ansible-playbook ${decksible_path}/playbooks/configure-ssh.yml --extra-vars='ansible_become_pass=deck'
-ansible-playbook ${decksible_path}/playbooks/install-flatpaks.yml
-ansible-playbook ${decksible_path}/playbooks/install-decky-loader.yml --extra-vars='ansible_become_pass=deck'
-ansible-playbook ${decksible_path}/playbooks/install-emudeck.yml --extra-vars='ansible_become_pass=deck'
+ansible-playbook ${decksible_path}/playbooks/main.yml --extra-vars='ansible_become_pass=deck'
 
 # We are done! Change wallpaper to indicate success
 
